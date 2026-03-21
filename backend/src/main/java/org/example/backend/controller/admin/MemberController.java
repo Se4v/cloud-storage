4e4695d22bd7d37c973c110320d96cc7bef8b165
tanta1ize <631350716@qq.com>
2026-03-21 17:00:50 +0800

    fix

--- backend/src/main/java/org/example/backend/controller/admin/MemberController.java
+++ backend/src/main/java/org/example/backend/controller/admin/MemberController.java
@@ -4,16 +4,14 @@ import org.example.backend.common.Result;
 import org.example.backend.model.args.CreateMemberArgs;
 import org.example.backend.model.args.DeleteMemberArgs;
 import org.example.backend.model.args.UpdateMemberArgs;
+import org.example.backend.model.entity.Node;
+import org.example.backend.model.entity.Role;
 import org.example.backend.model.view.MemberNodeView;
 import org.example.backend.model.view.MemberRoleView;
 import org.example.backend.model.view.MemberView;
-import org.example.backend.model.view.RoleView;
 import org.example.backend.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,32 +22,50 @@ public class MemberController {
     private MemberService memberService;
 
     @PostMapping("/create")
-    public Result<Void> createMember(CreateMemberArgs args) {
-
+    public Result<Void> createMember(@RequestBody CreateMemberArgs args) {
+        memberService.createMember(args);
+        return Result.success("");
     }
 
     @PostMapping("/delete")
-    public Result<Void> deleteMembers(DeleteMemberArgs args) {
-
+    public Result<Void> deleteMembers(@RequestBody DeleteMemberArgs args) {
+        memberService.deleteMembers(args);
+        return Result.success("");
     }
 
     @PostMapping("/update")
-    public Result<Void> updateMember(UpdateMemberArgs args) {
-
+    public Result<Void> updateMember(@RequestBody UpdateMemberArgs args) {
+        memberService.updateMember(args);
+        return Result.success("");
     }
 
     @GetMapping("/all")
     public Result<List<MemberView>> listAllMembers() {
-
+        List<MemberView> memberViews = memberService.listAllMembers();
+        return Result.success(memberViews);
     }
 
     @GetMapping("/role")
     public Result<List<MemberRoleView>> listOrgRoles() {
-
+        List<Role> roles = memberService.listOrgRoles();
+        List<MemberRoleView> memberRoleViews = roles.stream()
+                .map(role -> MemberRoleView.builder()
+                        .id(role.getId())
+                        .name(role.getName())
+                        .build())
+                .toList();
+        return Result.success(memberRoleViews);
     }
 
     @GetMapping("/org")
     public Result<List<MemberNodeView>> listOrgNodes() {
-
+        List<Node> nodes = memberService.listOrgNodes();
+        List<MemberNodeView> memberNodeViews = nodes.stream()
+                .map(node -> MemberNodeView.builder()
+                        .id(node.getId())
+                        .name(node.getNodeName())
+                        .build())
+                .toList();
+        return Result.success(memberNodeViews);
     }
 }
--- backend/src/main/java/org/example/backend/service/MemberService.java
+++ backend/src/main/java/org/example/backend/service/MemberService.java
@@ -1,6 +1,7 @@
 package org.example.backend.service;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import org.example.backend.common.exception.BusinessException;
 import org.example.backend.mapper.MemberMapper;
 import org.example.backend.mapper.NodeMapper;
@@ -18,7 +19,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class MemberService {
@@ -37,18 +39,24 @@ public class MemberService {
 
         // 查询用户是否存在
         LambdaQueryWrapper<User> userQuery = new LambdaQueryWrapper<>();
-        userQuery.eq(User::getUsername, args.getUsername());
+        userQuery.eq(User::getUsername, args.getUsername())
+                .eq(User::getEnabled, 1)
+                .eq(User::getDeleted, 0);
         User user = userMapper.selectOne(userQuery);
         if (user == null) throw new BusinessException("<UNK>");
 
         // 查询角色是否存在
         LambdaQueryWrapper<Role> roleQuery = new LambdaQueryWrapper<>();
-        roleQuery.eq(Role::getId, args.getRoleId());
+        roleQuery.eq(Role::getId, args.getRoleId())
+                .eq(Role::getEnabled, 1)
+                .eq(Role::getDeleted, 0);
         if (roleMapper.selectOne(roleQuery) == null) throw new BusinessException("<UNK>");
 
         // 查询组织节点是否存在
         LambdaQueryWrapper<Node> nodeQuery = new LambdaQueryWrapper<>();
-        nodeQuery.eq(Node::getId, args.getNodeId());
+        nodeQuery.eq(Node::getId, args.getNodeId())
+                .eq(Node::getEnabled, 1)
+                .eq(Node::getDeleted, 0);
         if (nodeMapper.selectOne(nodeQuery) == null) throw new BusinessException("<UNK>");
 
         int memberInsertCount = memberMapper.insert(Member.builder()
@@ -62,22 +70,112 @@ public class MemberService {
     @Transactional
     public void deleteMembers(DeleteMemberArgs args) {
         if (args == null) throw new BusinessException("");
+
+        List<Long> memberIds = args.getMemberIds();
+        if (memberIds == null) throw new BusinessException("");
+
+        LambdaUpdateWrapper<Member> memberUpdate = new LambdaUpdateWrapper<>();
+        memberUpdate.in(Member::getId, memberIds);
+        int memberDeleteCount = memberMapper.update(memberUpdate);
+        if (memberDeleteCount != memberIds.size()) throw new BusinessException("<UNK>");
     }
 
     @Transactional
     public void updateMember(UpdateMemberArgs args) {
         if (args == null) throw new BusinessException("");
+
+        // 查询成员信息是否存在
+        LambdaQueryWrapper<Member> memberQuery = new LambdaQueryWrapper<>();
+        memberQuery.eq(Member::getId, args.getMemberId()).eq(Member::getDeleted, 0);
+        if (memberMapper.selectOne(memberQuery) == null) throw new BusinessException("<UNK>");
+
+        // 更新信息
+        LambdaUpdateWrapper<Member> memberUpdate = new LambdaUpdateWrapper<>();
+        memberUpdate.set(args.getNodeId() != null, Member::getNodeId, args.getNodeId())
+                .set(args.getRoleId() != null, Member::getRoleId, args.getRoleId())
+                .eq(Member::getId, args.getMemberId());
+        if (memberMapper.update(memberUpdate) != 1) throw new BusinessException("<UNK>");
     }
 
     public List<MemberView> listAllMembers() {
+        LambdaQueryWrapper<Member> memberQuery = new LambdaQueryWrapper<>();
+        memberQuery.eq(Member::getDeleted, 0);
+        List<Member> members = memberMapper.selectList(memberQuery);
+        if (members == null) return List.of();
 
+        Set<Long> userIds = new HashSet<>();
+        Set<Long> roleIds = new HashSet<>();
+        Set<Long> nodeIds = new HashSet<>();
+        members.forEach(member -> {
+            userIds.add(member.getUserId());
+            roleIds.add(member.getRoleId());
+            nodeIds.add(member.getNodeId());
+        });
+
+        // 查询用户信息
+        Map<Long, User> userMap = new HashMap<>();
+        if (!userIds.isEmpty()) {
+            LambdaQueryWrapper<User> userQuery = new LambdaQueryWrapper<>();
+            userQuery.eq(User::getEnabled, 1).eq(User::getDeleted, 0).in(User::getId, userIds);
+            List<User> users = userMapper.selectList(userQuery);
+            userMap.putAll(
+                    users.stream().collect(Collectors.toMap(User::getId, user -> user, (k1, k2) -> k1))
+            );
+
+        }
+
+        // 查询角色信息
+        Map<Long, Role> roleMap = new HashMap<>();
+        if (!roleIds.isEmpty()) {
+            LambdaQueryWrapper<Role> roleQuery = new LambdaQueryWrapper<>();
+            roleQuery.eq(Role::getDeleted, 0).eq(Role::getEnabled, 1).in(Role::getId, roleIds);
+            List<Role> roles = roleMapper.selectList(roleQuery);
+            roleMap.putAll(
+                    roles.stream().collect(Collectors.toMap(Role::getId, role -> role, (k1, k2) -> k1))
+            );
+        }
+
+        // 查询节点信息
+        Map<Long, Node> nodeMap = new HashMap<>();
+        if (!nodeIds.isEmpty()) {
+            LambdaQueryWrapper<Node> nodeQuery = new LambdaQueryWrapper<>();
+            nodeQuery.eq(Node::getDeleted, 0).eq(Node::getEnabled, 1).in(Node::getId, nodeIds);
+            List<Node> nodes = nodeMapper.selectList(nodeQuery);
+            nodeMap.putAll(
+                    nodes.stream().collect(Collectors.toMap(Node::getId, node -> node, (k1, k2) -> k1))
+            );
+        }
+
+        List<MemberView> memberViews = members.stream()
+                .map(member -> {
+                    User user = userMap.get(member.getUserId());
+                    Role role = roleMap.get(member.getRoleId());
+                    Node node = nodeMap.get(member.getNodeId());
+                    return MemberView.builder()
+                            .id(member.getId())
+                            .username(user != null ? user.getUsername() : "")
+                            .realName(user != null ? user.getRealName() : "")
+                            .roleName(role != null ? role.getName() : "")
+                            .nodeName(node != null ? node.getNodeName() : "")
+                            .build();
+                })
+                .collect(Collectors.toList());
+
+        return memberViews;
     }
 
     public List<Role> listOrgRoles() {
-
+        LambdaQueryWrapper<Role> roleQuery = new LambdaQueryWrapper<>();
+        roleQuery.eq(Role::getType, 2)
+                .eq(Role::getEnabled, 1)
+                .eq(Role::getDeleted, 0);
+        return roleMapper.selectList(roleQuery);
     }
 
     public List<Node> listOrgNodes() {
-
+        LambdaQueryWrapper<Node> nodeQuery = new LambdaQueryWrapper<>();
+        nodeQuery.eq(Node::getEnabled, 1)
+                .eq(Node::getDeleted, 0);
+        return nodeMapper.selectList(nodeQuery);
     }
 }
